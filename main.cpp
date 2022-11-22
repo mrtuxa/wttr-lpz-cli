5f21d420c3594ae1ca0e23c4122ad984911eca6f
tuxa <69870860+mrtuxa@users.noreply.github.com>
2022-11-23 00:18:31 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cstdlib>
+
+int main() {
+    int output;
+    printf("Checking if processor is avaible...");
+    if (system(NULL)) puts ("Ok");
+        else exit (EXIT_FAILURE);
+    output=system("curl https://wttr.in/Leipzig");
+    printf("\n%d", output);
+}
